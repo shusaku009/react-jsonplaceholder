e1ae990f4cccbf642122a3356453f2fe37850a0a
shusaku009 <shusaku009@gmail.com>
2023-11-04 16:44:27 +0900

    add: JSONPlaceholderからaxiosでデータを取得

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import axios from "axios"
 import './App.css';
 
-function App() {
+export default function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+    }).catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+      console.log(res.data);
+    }).catch((err) => console.log(err));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
-
-export default App;
